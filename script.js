a683f37eac614fbed3e755ca517375a6c5268885
Ninabesse3 <148626543+Ninabesse3@users.noreply.github.com>
2025-10-15 11:39:05 +0200

    Update script.js
    ajout les trois mousquetaires

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ const cartes = [
         image: "images/Sous_la_Seine.jpg",
         texte: "Été 2024, Paris accueille pour la première fois les championnats du monde de triathlon sur la Seine. Sophia, brillante scientifique, est alertée par Mika, une jeune activiste dévouée à l'écologie, de la présence d'un grand requin dans les profondeurs du fleuve. Elles n'ont d'autre choix que de faire équipe avec Adil, commandant de la police fluviale pour éviter un bain de sang au cœur de la ville.",
     },
+     {
+        id: "2",
+        titre: "Les Trois mousquetaires : D'artagnan",
+        image: "https://img.bertrand.pt/images/os-tres-mosqueteiros-dartagnan-alexandre-dumas/NDV8MjkzNTk3NTR8MjU3NTc3NTV8MTY5NTgwNTEyMDAwMA==/500x",
+        texte: "Les Trois Mousquetaires raconte l'histoire de D'Artagnan, un jeune Gascon de 18 ans qui se rend à Paris pour devenir mousquetaire. Il se lie d'amitié avec Athos, Porthos et Aramis, les célèbres mousquetaires du roi Louis XIII, et ensemble, ils se battent pour défendre l'honneur et la justice dans un royaume divisé par les guerres de religion et menacé par l'Angleterre. Dans cette aventure, ils affrontent des complots politiques et des duels, tout en cherchant à protéger la reine.",
+    },
 ];
 const carte = cartes.find(c => c.id === id);
 
@@ -21,4 +27,5 @@ if (carte) {
     `;
 } else {
     container.innerHTML = "<p>Carte non trouvée.</p>";
+
 }
