0e915e4521504a8f2f68ef7c4bfe59d2199c36fd
Nina Ratsimihah <nratsimi@gmail.com>
2025-11-05 11:11:56 +0100

    ajout map avec plusieurs pins

--- script.js
+++ script.js
@@ -9,12 +9,21 @@ const cartes = [
         titre: "Sous la Seine",
         image: "images/Sous_la_Seine.jpg",
         texte: "Été 2024, Paris accueille pour la première fois les championnats du monde de triathlon sur la Seine. Sophia, brillante scientifique, est alertée par Mika, une jeune activiste dévouée à l'écologie, de la présence d'un grand requin dans les profondeurs du fleuve. Elles n'ont d'autre choix que de faire équipe avec Adil, commandant de la police fluviale pour éviter un bain de sang au cœur de la ville.",
+        pins: [
+            {coords: [48.82623, 2.36085], label: "lieu"},
+        ],
+        zoom: 16,
     },
      {
         id: "2",
         titre: "Les Trois mousquetaires : D'artagnan",
         image: "https://img.bertrand.pt/images/os-tres-mosqueteiros-dartagnan-alexandre-dumas/NDV8MjkzNTk3NTR8MjU3NTc3NTV8MTY5NTgwNTEyMDAwMA==/500x",
         texte: "Les Trois Mousquetaires raconte l'histoire de D'Artagnan, un jeune Gascon de 18 ans qui se rend à Paris pour devenir mousquetaire. Il se lie d'amitié avec Athos, Porthos et Aramis, les célèbres mousquetaires du roi Louis XIII, et ensemble, ils se battent pour défendre l'honneur et la justice dans un royaume divisé par les guerres de religion et menacé par l'Angleterre. Dans cette aventure, ils affrontent des complots politiques et des duels, tout en cherchant à protéger la reine.",
+        pins: [
+            {coords: [48.8425, 2.3393], label: "lieu 1"},
+            {coords: [48.82623, 2.36085], label: "lieu 2"},
+        ],
+        zoom: 16,
     },
 ];
 //Parcours le tableau de cartes pour trouver l'id de la carte
@@ -30,8 +39,24 @@ if (carte) {
     <h2>${carte.titre}</h2>
     <img src="${carte.image}" alt="${carte.titre}" style="width:400px;">
     <p>${carte.texte}</p>
+    <div id="map" style="width:400px;height:300px;margin-top:20px;border-radius:10px;"></div>
     `;
+    const map = L.map("map"); 
+    const latlngs = carte.pins.map(pin => pin.coords);
+    map.fitBounds(latlngs, {padding: [50, 50]});
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+  }).addTo(map);
+
+  map.attributionControl.setPrefix(false);
+
+carte.pins.forEach(pin => {
+    L.marker(pin.coords).addTo(map).bindPopup(`${pin.label}`);
+});
+
 } else {
     container.innerHTML = "<p>Carte non trouvée.</p>";
-
+    container.innerHTML = "<p>Carte non trouvée.</p>";
 }
